d06675017a0437a4c4914f55b95ad932defde68c
Alessio Filippucci <info@alessiofilippucci.it>
2024-05-24 12:56:03 +0200

    Added Radzen framework

---
+++ RadzenApp/Models/News.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RadzenApp.Models
+{
+    public class News
+    {
+        public int Pos { get; set; }
+        public string IdNews { get; set; } = null!;
+        [Required]
+        public string? Titolo { get; set; }
+        public string DataPubblicazione { get; set; } = null!;
+        public string TipiCod { get; set; } = null!;
+        [Required]
+        [StringLength(5)]
+        public string Autore { get; set; } = null!;
+        public string IconaCategoria { get; set; } = null!;
+        public bool LinkTargetBlank { get; set; }
+        public string Login { get; set; } = null!;
+        public string Pagamento { get; set; } = null!;
+        public string PathDettaglio { get; set; } = null!;
+        public string PathDettaglioRelative { get; set; } = null!;
+        public string PathImg { get; set; } = null!;
+        public string PathImgPortrait { get; set; } = null!;
+        public string PathVideo { get; set; } = null!;
+        public string Sommario { get; set; } = null!;
+        public string SommarioHtml { get; set; } = null!;
+        public string Tag { get; set; } = null!;
+        public string TipoDesc { get; set; } = null!;
+        public string TipoPath { get; set; } = null!;
+        public string TitoloHp { get; set; } = null!;
+        public string TitoloHtml { get; set; } = null!;
+    }
+}
---
+++ RadzenApp/Services/NewsService.cs
@@ -0,0 +1,40 @@
+﻿using RadzenApp.Models;
+using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+
+namespace RadzenApp.Services
+{
+    public class NewsService
+    {
+        protected readonly ILogger<NewsService> _logger;
+
+        protected readonly RestClient _client;
+
+        public NewsService(
+            ILogger<NewsService> logger)
+        {
+            _logger = logger;
+
+            _client = new RestClient("https://ws3.class.it/CE.Content/Content.svc/", configureSerialization: s => s.UseNewtonsoftJson());
+        }
+
+        public async Task<List<News>> GetDataAsync(string idBlocco = "1")
+        {
+            var request = new RestRequest("get-news-blocco-dettaglio", Method.Get);
+            request.AddQueryParameter("id_blocco", idBlocco);
+            var queryResult = await _client.ExecuteAsync<List<News>>(request);
+
+            if (queryResult.IsSuccessStatusCode)
+            {
+                return queryResult.Data ?? [];
+            }
+            return [];
+        }
+
+        public async Task<News?> GetDataByIdAsync(string id)
+        {
+            var newsList = await GetDataAsync();
+            return newsList.FirstOrDefault(x => x.IdNews == id);
+        }
+    }
+}
