f00ccc521fbdfb9d4829729816a0bd00564b05bd
Alessio Filippucci <info@alessiofilippucci.it>
2024-05-24 11:24:33 +0200

    Add JSInterop: JS -> NET

---
+++ BlazorServer/Services/DotNetToJSSample.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorServer.Services;
+
+public class DotNetToJSSample
+{
+    private string name;
+
+    public string Name { get => name; set => name = value; }
+
+    [JSInvokable]
+    public Task<List<int>> GetDataAsync()
+    {
+        List<int> data = new List<int>();
+
+        var rdm = new Random();
+
+        foreach (var _ in Enumerable.Range(1, 10))
+        {
+            data.Add(rdm.Next(0, 100));
+        }
+
+        return Task.FromResult(data);
+    }
+
+    [JSInvokable]
+    public string GetWelcomeMessage() => $"Welcome, {name}";
+}
--- BlazorServer/wwwroot/scripts/script.js
+++ BlazorServer/wwwroot/scripts/script.js
@@ -6,3 +6,39 @@
 window.getName = () => {
     return prompt("Qual è il tuo nome?");
 }
+
+//call NET static method
+
+window.getDataFromNet = () => {
+    //setInterval(() => {
+    DotNet.invokeMethodAsync("BlazorServer", "GetData")
+        .then(data => {
+            console.log(data);
+        })
+    //}, 1000)
+}
+
+//call NET method in class
+
+class DotNetToJSSample {
+    static dotNetInstance;
+
+    static setDotNetInstance(instance) {
+        DotNetToJSSample.dotNetInstance = instance;
+    }
+
+    static getData() {
+        DotNetToJSSample.dotNetInstance.invokeMethodAsync("GetDataAsync").then(data => {
+            console.log(data);
+        });
+    }
+
+    static getWelcomeMessage() {
+        console.log(DotNetToJSSample.dotNetInstance);
+        DotNetToJSSample.dotNetInstance.invokeMethodAsync("GetWelcomeMessage").then(data => {
+            alert(data);
+        });
+    }
+}
+
+window.DotNetToJSSample = DotNetToJSSample;
